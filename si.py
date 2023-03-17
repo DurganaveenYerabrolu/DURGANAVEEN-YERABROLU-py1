33389a59c8cfb643fdd6ed803f5424453664750b
DurganaveenYerabrolu <128154140+DurganaveenYerabrolu@users.noreply.github.com>
2023-03-17 21:25:12 +0530

    Update si.py

--- si.py
+++ si.py
@@ -1,5 +1,5 @@
 P=int(input("enter the principle amount:"))
 T=float(input("enter the time :"))
 R=float(input("enter the rate:"))
-Simple Interest=(P*T*R)/100
-print(Simple Interest)
+SimpleInterest=(P*T*R)/100
+print(SimpleInterest)
